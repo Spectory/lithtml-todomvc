805ad42eafbc29b5b834678c9b443776bc48234f
Gil Meir <gil@spectory.com>
2019-02-16 06:25:27 +0200

    hide footer

--- app/components/App.js
+++ app/components/App.js
@@ -2,13 +2,16 @@ import { html } from 'lit-html'
 import { Header } from './Header'
 import { Main } from './Main'
 import { Footer } from './Footer'
+import { itemLeftCount } from '../store/util'
 
 export function App (state) {
   return html`
     <section class='todoapp'>
       ${Header()}
       ${Main(state.todos, state.visibilityFilter)}
+      ${itemLeftCount(state.todos) > 0 ? html`
       ${Footer(state.todos, state.visibilityFilter)}
+      ` : html``}
     </section>
   `
 }
