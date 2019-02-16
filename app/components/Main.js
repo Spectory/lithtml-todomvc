3227760febd7dd43a31deb18cd4fe196d3bda784
Gil Meir <gil@spectory.com>
2019-02-16 07:22:08 +0200

    move to utils

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,19 +1,8 @@
 import { html } from 'lit-html'
 import { Todo } from './Todo'
-import { VisibilityFilters, toggleAll } from '../store/actions'
+import { toggleAll } from '../store/actions'
 import { store } from '../store/store'
-import { areAllItemsCompleted } from '../store/util'
-
-function todosByVisibilityFilter (todos, visibilityFilter) {
-  switch (visibilityFilter) {
-    case VisibilityFilters.SHOW_ALL:
-      return todos
-    case VisibilityFilters.SHOW_ACTIVE:
-      return todos.filter((todo) => todo.completed !== true)
-    case VisibilityFilters.SHOW_COMPLETED:
-      return todos.filter((todo) => todo.completed === true)
-  }
-}
+import { areAllItemsCompleted, todosByVisibilityFilter } from '../store/util'
 
 function onClickToggleAll () {
   store.dispatch(toggleAll())
--- app/store/util.js
+++ app/store/util.js
@@ -1,3 +1,5 @@
+import { VisibilityFilters } from './actions'
+
 export function itemLeftCount (todos) {
   return todos.length
 }
@@ -8,4 +10,15 @@ export function itemCompletedCount (todos) {
 
 export function areAllItemsCompleted (todos) {
   return itemCompletedCount(todos) === itemLeftCount(todos) && itemLeftCount(todos) > 0
+}
+
+export function todosByVisibilityFilter (todos, visibilityFilter) {
+  switch (visibilityFilter) {
+    case VisibilityFilters.SHOW_ALL:
+      return todos
+    case VisibilityFilters.SHOW_ACTIVE:
+      return todos.filter((todo) => todo.completed !== true)
+    case VisibilityFilters.SHOW_COMPLETED:
+      return todos.filter((todo) => todo.completed === true)
+  }
 }
